2743538378de78e7c6bfae032f8f5f79b2138cce
Olena-Horobets <80359219+Olena-Horobets@users.noreply.github.com>
2021-09-08 23:11:26 +0300

    color switch feature added
    -markup, styles and functionality of color switch added;
    -spiral animation added;

---
+++ index.js
@@ -0,0 +1,54 @@
+const colors = [
+  "#42f5f2",
+  "#42f542",
+  "#e67002",
+  "#1bcf1e",
+  "#6920fa",
+  "#6920fa",
+  "#fa232e",
+  "#fcf803",
+  "#017503",
+];
+
+const refs = {
+  body: document.querySelector("body"),
+  spiralBox: document.querySelector(".spiral-box"),
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+};
+
+const colorSwitch = {
+  interval: 1000,
+  isACtive: false,
+  intervalId: null,
+};
+
+refs.startBtn.addEventListener("click", onStartBtnClick);
+refs.stopBtn.addEventListener("click", onStopBtnClick);
+
+function onStartBtnClick() {
+  if (colorSwitch.isACtive) {
+    return;
+  }
+
+  colorSwitch.isACtive = true;
+
+  colorSwitch.intervalId = setInterval(() => {
+    const style = getRandomColor(colors);
+    refs.body.setAttribute("style", "background-color: " + style + ";");
+  }, colorSwitch.interval);
+
+  refs.spiralBox.classList.add("animate");
+}
+
+function onStopBtnClick() {
+  colorSwitch.isACtive = false;
+  clearInterval(colorSwitch.intervalId);
+  refs.body.setAttribute("style", "background-color: #ffffff;");
+  refs.spiralBox.classList.remove("animate");
+}
+
+function getRandomColor(arr) {
+  const color = Math.floor(Math.random() * (0 + arr.length) - 0);
+  return arr[color];
+}
